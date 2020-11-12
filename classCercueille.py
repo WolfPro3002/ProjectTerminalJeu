e306259cc65697304f98ae800207d232be75a897
WolfPro3002 <72748704+WolfPro3002@users.noreply.github.com>
2020-11-12 19:10:12 +0100

    Add files via upload

--- classCercueille.py
+++ classCercueille.py
@@ -3,10 +3,10 @@ import pygame
 # Création d'une classe cercueille :
 class Cercueille(pygame.sprite.Sprite):
 
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load('Asset/Ancien Cercueil2.xcf')
+        self.image = pygame.image.load('Asset/Ancien Cercueil2.png')
         self.rect = self.image.get_rect()
         # Placement au milieu de l'écran :
-        self.rect.x = 540
-        self.rect.y = 378
+        self.rect.x = x # 540
+        self.rect.y = y # 378
